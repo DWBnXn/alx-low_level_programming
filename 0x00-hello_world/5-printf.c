b0dc3db2807ba123682094b7bd088a05cfdf37bc
Obiora Daniel <obioradaniel19@gmail.com>
2022-12-08 10:52:24 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main- Serves as entry point for the program
+ * Return: By defaulf zero if successful otherwise a non-zero value
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
